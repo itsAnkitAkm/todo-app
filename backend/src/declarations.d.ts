a844e8cf398bc8690f790090babde9fc80a35d43
itsAnkitAkm <{itsamkankit@gmail.com}+{itsAnkitAkm}@users.noreply.github.com>
2024-11-10 03:13:19 +0530

    add error check for deploy

---
+++ backend/src/declarations.d.ts
@@ -0,0 +1,7 @@
+declare module 'cookie-parser';
+declare module 'cors';
+declare module 'morgan';
+declare module 'bcryptjs';
+declare module 'nodemailer';
+//declare module 'jsonwebtoken';
+declare module 'validator';
